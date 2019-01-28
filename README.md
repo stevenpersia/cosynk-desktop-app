9d9a5c613addb3c744eca6673f3b9226ca4352f0
Steven Persia <persia.steven8@gmail.com>
2019-01-29 00:28:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Cosynk desktop app
---
+++ src/main/index.js
@@ -0,0 +1,92 @@
+import { app, BrowserWindow, Menu } from 'electron';
+import path from 'path';
+
+let mainWindow;
+
+function createMainWindow() {
+	const window = new BrowserWindow({
+		title: 'My App',
+		titleBarStyle: 'hidden',
+		width: 1200,
+		height: 800,
+		minWidth: 800,
+		minHeight: 600,
+		resizable: true,
+		icon: path.join(__dirname, 'assets/64x64.png'),
+		webPreferences: {
+			nodeIntegration: false
+		}
+	});
+
+	window.loadURL('http://cosynk.com/');
+	window.setMenu(null);
+	window.webContents.on('did-finish-load', function() {
+		window.webContents.insertCSS(
+			'.App-header{-webkit-app-region:drag;-webkit-user-select: none;}'
+		);
+	});
+	window.on('closed', () => {
+		mainWindow = null;
+	});
+
+	const template = [
+		{
+			label: 'Cosynk',
+			submenu: [
+				{
+					label: 'Exit',
+					click() {
+						app.quit();
+					}
+				}
+			]
+		},
+		{
+			label: 'View',
+			submenu: [{ role: 'reload' }, { role: 'togglefullscreen' }]
+		},
+		{ label: 'Window', submenu: [{ role: 'minimize' }, { role: 'close' }] },
+		{
+			label: 'Help',
+			submenu: [
+				{
+					label: "Cosynk's website",
+					click() {
+						require('electron').shell.openExternal('http://cosynk.com/');
+					}
+				}
+			]
+		}
+	];
+	const menu = Menu.buildFromTemplate(template);
+	Menu.setApplicationMenu(menu);
+
+	if (process.env.NODE_ENV !== 'production') {
+		window.webContents.openDevTools();
+	}
+
+	window.webContents.on('devtools-opened', () => {
+		window.focus();
+		setImmediate(() => {
+			window.focus();
+		});
+	});
+
+	return window;
+}
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (mainWindow === null) {
+		mainWindow = createMainWindow();
+	}
+});
+
+app.on('ready', () => {
+	mainWindow = createMainWindow();
+});
